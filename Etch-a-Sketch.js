7de8071477a938c50ec18f07471c227480f80311
Andre Bundal <andrecarlosbundal@gmail.com>
2025-10-19 15:10:32 +0800

    Managed to add loop for first row of boxes

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,6 +2,6 @@ const container = document.querySelector("#container");
 
 const box = document.createElement("div");
 
-for (let i = 0; i <= 16; i++) {
-    container.append(box);
+for (let i = 0; i <= 15; i++) {
+    container.append(box.cloneNode(true));
 }
