a92790dff405c0585297f8bd7d4631ba6f7590ca
Andre Bundal <andrecarlosbundal@gmail.com>
2025-10-27 18:59:53 +0800

    added function to create 16x16 grid

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,7 +1,19 @@
-const container = document.querySelector("#container");
+const sContainer = document.querySelector(".container")
+const container = document.createElement("div");
+container.setAttribute("class", "container")
 
 const box = document.createElement("div");
+box.setAttribute("class", "cldContainer");
+
+function appendNCopies(n, original, appendTo) {
+    for(let i = 0; i < n; i++) {
+        let clone = original.cloneNode(true);
+        appendTo.appendChild(clone);
+    }
+}
 
 for (let i = 0; i <= 15; i++) {
-    container.append(box.cloneNode(true));
+    sContainer.append(box.cloneNode(true));
 }
+
+appendNCopies(15, sContainer, document.body)
