a4728932b7c8cfdc14dbb2716c5ccbc48ec903a2
Andre Bundal <andrecarlosbundal@gmail.com>
2025-11-21 15:36:35 +0800

    Added comments explaining blocks of code
    Currently restructuring some functions

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,32 +1,53 @@
-const sContainer = document.querySelector(".container")
+const Container = document.querySelector(".container")
 
+const colBoxes = document.createElement("div")
+colBoxes.setAttribute("class", "sContainer")
+
+//create divs to be inserted into container
 const box = document.createElement("div");
-box.setAttribute("class", "cldContainer");
+box.setAttribute("class", "items");
 
-function appendNCopies(n, original, appendTo) {
+//function to append columns of boxes
+function generateCanvas(n=16) {
     for(let i = 0; i < n; i++) {
-        let clone = original.cloneNode(true);
-        appendTo.appendChild(clone);
+        let clone = colBoxes.cloneNode(true);
+        Container.appendChild(clone);
     }
 }
 
-for (let i = 0; i <= 15; i++) {
-    sContainer.append(box.cloneNode(true));
+//function to append rows of boxes
+function appendRows(n=16) {
+    for (let i = 0; i < n; i++) {
+        colBoxes.append(box.cloneNode(true));
+    }
 }
 
-appendNCopies(15, sContainer, document.body)
+//Function to create the whole canvas
 
-const canvas = document.querySelectorAll(".cldContainer")
+generateCanvas()
+
+
+//button function for requesting custom sized canvas
+const reqButton = document.querySelector("button");
+
+reqButton.addEventListener("click", function() {
+    let canvasSize = prompt("Please enter desired number of boxes");
+
+    if (isNaN(canvasSize) || canvasSize <= 0) {
+        alert("The input submitted is not a valid number")
+    } else if (canvasSize > 100) {
+        alert("The maximum number that can be accepted is 100")
+    } else {
+        wholeCanvas(canvasSize)
+    }
+})
+
+//main function for Etch a Sketch
+const canvas = document.querySelectorAll(".items")
 
 canvas.forEach((canva) => {
     canva.addEventListener("mouseout", function() {
         canva.style.backgroundColor = "black";
         console.log("Hovered!")
     })
-})
-
-const reqButton = document.querySelector("button");
-
-reqButton.addEventListener("click", function() {
-    
 })
